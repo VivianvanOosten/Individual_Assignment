463df8e5dcddfd2b38986b49756f00cac104ce3e
Vivian van Oosten <fif@amvi.nl>
2020-01-16 11:46:11 +0100

    python file converting the json dictionary to csv - India 2001
    only deaths and id, that's all

---
+++ csv_converting.py
@@ -0,0 +1,20 @@
+
+import csv
+import json
+
+with open('conflict_data/conflict_data_full_lined.json') as file:
+    conflict_data = json.load(file)
+
+india_2001 = []
+
+for incident in conflict_data:
+    if incident["country"] == "India":
+        if incident["year"]:
+            relevant_data = [incident["id"], incident["best"], incident["high"], incident["low"]]
+            india_2001.append(relevant_data)
+
+# print(india_2001)
+
+with open('conflict_data/relevant_data.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(india_2001)
