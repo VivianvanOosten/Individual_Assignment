f2c20c93c6b5e03b427b5c745f671cf883f1de6c
Vivian van Oosten <fif@amvi.nl>
2020-01-16 14:20:49 +0100

    py include years in csv - India

--- csv_converting.py
+++ csv_converting.py
@@ -5,16 +5,14 @@ import json
 with open('conflict_data/conflict_data_full_lined.json') as file:
     conflict_data = json.load(file)
 
-india_2001 = [["id", "best", "high", "low"]]
+india = [["id","year", "best", "high", "low"]]
 
 for incident in conflict_data:
     if incident["country"] == "India":
-        if incident["year"]:
-            relevant_data = [incident["id"], incident["best"], incident["high"], incident["low"]]
-            india_2001.append(relevant_data)
+        relevant_data = [incident["id"],incident["year"], incident["best"], incident["high"], incident["low"]]
+        india.append(relevant_data)
 
-# print(india_2001)
-
-with open('conflict_data/relevant_data.csv', 'w') as csvfile:
+with open('conflict_data/relevant_data_years.csv', 'w') as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerows(india_2001)
+    writer.writerows(india)
+
